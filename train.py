0f30ae29c65a4d4ea7535b874940a838964250f8
SouhailaK <114444988+SouhailaK@users.noreply.github.com>
2025-03-31 18:47:05 +0200

    Update2 train.py

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ logger = logging.getLogger(__name__)
 
 
 def train(hyp, opt, device, tb_writer=None):
-    warnings.simplefilter("ignore", category=FutureWarning)
+    warnings.simplefilter("ignore")
     logger.info(colorstr('hyperparameters: ') + ', '.join(f'{k}={v}' for k, v in hyp.items()))
     save_dir, epochs, batch_size, total_batch_size, weights, rank, freeze = \
         Path(opt.save_dir), opt.epochs, opt.batch_size, opt.total_batch_size, opt.weights, opt.global_rank, opt.freeze
